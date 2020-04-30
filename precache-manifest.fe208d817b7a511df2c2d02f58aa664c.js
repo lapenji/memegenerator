e950fbdd09a77c0bae82b07620ac78f535d0fe62
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-04-30 19:21:50 +0200

    Updates

---
+++ precache-manifest.fe208d817b7a511df2c2d02f58aa664c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01ce6a134064966006e6bcf82e91641",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "6c736c6527c11c523218",
+    "url": "/memegenerator/static/css/main.d83ab3b3.chunk.css"
+  },
+  {
+    "revision": "66f879ed4e57170292c5",
+    "url": "/memegenerator/static/js/2.b01ef696.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memegenerator/static/js/2.b01ef696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c736c6527c11c523218",
+    "url": "/memegenerator/static/js/main.01335ac5.chunk.js"
+  },
+  {
+    "revision": "695d7c0f9feca02455c5",
+    "url": "/memegenerator/static/js/runtime-main.06d47b28.js"
+  },
+  {
+    "revision": "a9a07d89293efb75a57bb31873d70330",
+    "url": "/memegenerator/static/media/cina.a9a07d89.jpeg"
+  },
+  {
+    "revision": "20197d04274470352543b56aab795372",
+    "url": "/memegenerator/static/media/cina2.20197d04.png"
+  },
+  {
+    "revision": "c19f62bb48691d4a0ec969e0d4c86710",
+    "url": "/memegenerator/static/media/claretto.c19f62bb.jpg"
+  },
+  {
+    "revision": "2619645c8ac435b9b9ab92378a042a52",
+    "url": "/memegenerator/static/media/icona.2619645c.png"
+  }
+]);
